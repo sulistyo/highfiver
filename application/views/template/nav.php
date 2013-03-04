6cee8c90b8e57b00be5ae27e14b64721d5ae73b5
root <root@server10.esolutech.com>
2013-03-03 22:10:57 -0500

    add github url

--- application/views/template/nav.php
+++ application/views/template/nav.php
@@ -2,5 +2,5 @@
 	<li class="<?php echo isActive($pageName,"main")?>"><a href="<?php echo  base_url()?>">Top Five</a></li>
 	<li class="<?php echo isActive($pageName,"about")?>"><a href="<?php echo  base_url()."about"; ?>">About</a></li>
 	<li class="<?php echo isActive($pageName,"test")?>"><a href="<?php echo  base_url()."test"; ?>">Unit Test</a></li>
-	<li><a href="<?php echo  base_url();?>">GitHub</a></li>
+	<li><a href="https://github.com/sulistyo/highfiver">GitHub</a></li>
 </ul>
